b62ca036c56969d1ebd987ec97d764dd3f01a762
Natália Leonel <leonel.natalia@aluno.ifsp.edu.br>
2022-08-26 15:27:00 -0300

    projeto chave primaria composta

---
+++ cahve-primaria-composta/cahvae-composta-idClass/src/Principal.java
@@ -0,0 +1,9 @@
+
+import javax.persistence.Persistence;
+
+
+public class Principal {
+    public static void main(String[] args) {
+        Persistence.createEntityManagerFactory("cahve-composta-idClassPU");
+    }
+}
---
+++ cahve-primaria-composta/cahvae-composta-idClass/src/br/edu/ifsp/pep/modelo/Telefone.java
@@ -0,0 +1,43 @@
+package br.edu.ifsp.pep.modelo;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "telefone")
+@IdClass(TelefonePK.class)
+public class Telefone implements Serializable{
+    @Id
+    @Column(name = "ddd")
+    private int ddd;
+    
+    @Id
+    @Column(name = "numero")
+    private String numero;
+
+    @Column(name = "descricao", length = 40)
+    private String descricao;
+    
+    public int getDdd() {
+        return ddd;
+    }
+
+    public void setDdd(int ddd) {
+        this.ddd = ddd;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+    
+    
+        
+}
